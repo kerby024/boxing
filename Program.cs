6e1dfe366e214e9d142ebcb59df079b94a78d32f
Blake <kerby024@gmail.com>
2018-07-25 14:39:44 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> trial = new List<object>();
+            trial.Add(7);
+            trial.Add(28);
+            trial.Add(-1);
+            trial.Add(true);
+            trial.Add("chair");
+            int sum = 0;
+            for(var i =0; i <trial.Count; i++)
+            {
+                // Console.WriteLine(trial[i]);
+            if(trial[i] is int){
+                sum += (int)trial[i];
+                Console.WriteLine(sum);
+            }
+            }
+        }
+    }
+}
